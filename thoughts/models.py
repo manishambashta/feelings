b5717d728b855909a76f03389029e4f67120b4bf
Manish Kumar <manish.kumar@networkbulls.in>
2018-03-29 06:56:32 +0530

    basic thoughts app with admin

--- thoughts/models.py
+++ thoughts/models.py
@@ -3,32 +3,38 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-CONDITIONS=(
+CONDITIONS = (
 
-    (1, 'Ecstatic'),
-    (2, 'Passionate'),
-    (3, 'Happy'),
-    (4, 'Optimistic'),
-    (5, 'Content'),
-    (6, 'Bored'),
-    (7, 'Pessimistic'),
-    (8, 'Frustrated'),
-    (9, 'Overwhelmed'),
-    (10, 'Disappointed'),
-    (11, 'Worried'),
-    (12, 'Angry'),
-    (13, 'Jealous'),
-    (14, 'Insecure'),
-    (15, 'Guilty'),
-    (16, 'Fear'),
-    (17, 'Grief'),
-    (18, 'Despiar'),
+    (0, 'Ecstatic'),
+    (20, 'Passionate'),
+    (30, 'Happy'),
+    (40, 'Optimistic'),
+    (50, 'Content'),
+    (60, 'Bored'),
+    (62, 'Tired'),
+    (65, 'Hungry'),
+    (70, 'Pessimistic'),
+    (80, 'Frustrated'),
+    (90, 'Overwhelmed'),
+    (100, 'Disappointed'),
+    (110, 'Worried'),
+    (120, 'Angry'),
+    (130, 'Jealous'),
+    (140, 'Insecure'),
+    (150, 'Guilty'),
+    (160, 'Fear'),
+    (170, 'Grief'),
+    (180, 'Despair'),
+    (190, 'Paranoia'),
 
 )
 
 
 class Thought(models.Model):
-    user = models.ForeignKey(User, related_name="thoughts")
+    user = models.ForeignKey(User, related_name="thoughts", on_delete=models.CASCADE)
     recorded_at = models.DateTimeField(default=timezone.now, editable=False)
-    conditions = models.IntegerField(choices=CONDITIONS)
+    condition = models.IntegerField(choices=CONDITIONS)
     notes = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return '{}: {}'.format(self.recorded_at.strftime('%Y-%m-%d %H:%M:%S'), self.get_condition_display())
