16615653c4dc36ca17aeeacf123ffaf51ecec1c3
Manish Kumar <manish.kumar@networkbulls.in>
2018-03-31 13:30:06 +0530

    dashboard placeholder and settings

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from django.urls import include
 
+from . import views
+
 urlpatterns = [
+
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url('^', include('django.contrib.auth.urls')),
 ]
