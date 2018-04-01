e5bd4d429a176e8cc17b9680671581c3643bb339
Manish Kumar <manish.kumar@networkbulls.in>
2018-04-01 08:54:24 +0530

    signup view and route

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^logout/$', views.LogoutView.as_view(), name="logout"),
+    url(r'^signup/$', views.SignupView.as_view(), name="signup"),
     url('^', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 # Create your views here.
 
 from . import forms
@@ -20,3 +21,9 @@ class LogoutView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         logout(self.request)
         return HttpResponseRedirect(reverse('home'))
+
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('users:dashboard')
